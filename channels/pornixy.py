e434a09e0487e7b939a71d7edf450693215d4d9e
paeznet <jsaezu@wanadoo.es>
2025-06-19 10:38:51 +0200

    +18

---
+++ channels/pornixy.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+# -*- Channel Pornixy -*-
+# -*- Created for Alfa-addon -*-
+# -*- By the Alfa Develop Group -*-
+
+import sys
+PY3 = False
+if sys.version_info[0] >= 3: PY3 = True; unicode = str; unichr = chr; long = int; _dict = dict
+
+from lib import AlfaChannelHelper
+if not PY3: _dict = dict; from AlfaChannelHelper import dict
+from AlfaChannelHelper import DictionaryAdultChannel
+from AlfaChannelHelper import re, traceback, time, base64, xbmcgui
+from AlfaChannelHelper import Item, servertools, scrapertools, jsontools, get_thumb, config, logger, filtertools, autoplay
+
+IDIOMAS = {}
+list_language = list(set(IDIOMAS.values()))
+list_quality = []
+list_quality_movies = []
+list_quality_tvshow = []
+list_servers = []
+forced_proxy_opt = 'ProxySSL'
+
+
+canonical = {
+             'channel': 'pornixy', 
+             'host': config.get_setting("current_host", 'pornixy', default=''), 
+             'host_alt': ["https://pornixy.com/"], 
+             'host_black_list': [], 
+             'set_tls': True, 'set_tls_min': True, 'retries_cloudflare': 1, 'forced_proxy_ifnot_assistant': forced_proxy_opt, 'cf_assistant': False, 
+             'CF': False, 'CF_test': False, 'alfa_s': True
+            }
+host = canonical['host'] or canonical['host_alt'][0]
+
+timeout = 5
+kwargs = {}
+debug = config.get_setting('debug_report', default=False)
+movie_path = ''
+tv_path = ''
+language = []
+url_replace = []
+
+finds = {'find': dict([('find', [{'tag': ['main'], 'role': ['main']}]),
+                       ('find_all', [{'tag': ['div'], 'class': ['col']}])]),     #'id': re.compile(r"^browse_\d+")}]},
+         'categories': dict([('find', [{'tag': ['main'], 'role': ['main']}]),
+                             ('find_all', [{'tag': ['div'], 'class': ['col', 'col-6']}])]),
+         'search': {}, 
+         'get_quality': {}, 
+         'get_quality_rgx': '', 
+         'next_page': {'find_all': [{'tag': ['a'], 'class': ['btn-page'], '@POS': [-1], '@ARG': 'href'}]},
+         'next_page_rgx': [['&page=\d+', '&page=%s']], 
+         'last_page': {}, 
+         'plot': {}, 
+         'findvideos': {},
+         'title_clean': [['[\(|\[]\s*[\)|\]]', ''],['(?i)\s*videos*\s*', '']],
+         'quality_clean': [['(?i)proper|unrated|directors|cut|repack|internal|real|extended|masted|docu|super|duper|amzn|uncensored|hulu', '']],
+         'url_replace': [], 
+         'profile_labels': {
+                           },
+         'controls': {'url_base64': False, 'cnt_tot': 36, 'reverse': False, 'profile': 'default'},  ##'jump_page': True, ##Con last_page  aparecerá una línea por encima de la de control de página, permitiéndote saltar a la página que quieras
+         'timeout': timeout}
+AlfaChannel = DictionaryAdultChannel(host, movie_path=movie_path, tv_path=tv_path, movie_action='play', canonical=canonical, finds=finds, 
+                                     idiomas=IDIOMAS, language=language, list_language=list_language, list_servers=list_servers, 
+                                     list_quality_movies=list_quality_movies, list_quality_tvshow=list_quality_tvshow, 
+                                     channel=canonical['channel'], actualizar_titulos=True, url_replace=url_replace, debug=debug)
+
+
+def mainlist(item):
+    logger.info()
+    itemlist = []
+    
+    itemlist.append(Item(channel=item.channel, title="Nuevos" , action="list_all", url=host + "videos?page=1"))
+    itemlist.append(Item(channel=item.channel, title="Canal" , action="section", url=host + "channels/", extra="Canal"))
+    itemlist.append(Item(channel=item.channel, title="Pornstars" , action="section", url=host + "pornstars/", extra="PornStar"))
+    itemlist.append(Item(channel=item.channel, title="Amateur Models" , action="section", url=host + "models/", extra="PornStar"))
+    itemlist.append(Item(channel=item.channel, title="Categorias" , action="section", url=host + "categories/", extra="Categorias"))
+    itemlist.append(Item(channel=item.channel, title="Buscar", action="search"))
+    
+    return itemlist
+
+
+def section(item):
+    logger.info()
+    
+    return AlfaChannel.section(item, **kwargs)
+
+
+def list_all(item):
+    logger.info()
+    
+    # return AlfaChannel.list_all(item, **kwargs)
+    return AlfaChannel.list_all(item, matches_post=list_all_matches, **kwargs)
+
+
+def list_all_matches(item, matches_int, **AHkwargs):
+    logger.info()
+    matches = []
+    
+    findS = AHkwargs.get('finds', finds)
+    
+    for elem in matches_int:
+        elem_json = {}
+        
+        try:
+            elem_json['url'] = elem.a.get('href', '')
+            elem_json['title'] = elem.a.get('title', '') \
+                                 or elem.find(class_='title').get_text(strip=True) if elem.find(class_='title') else ''
+            if not elem_json['title']:
+                elem_json['title'] = elem.img.get('alt', '')
+            
+            elem_json['thumbnail'] = elem.img.get('data-thumb_url', '') or elem.img.get('data-original', '') \
+                                     or elem.img.get('data-src', '') \
+                                     or elem.img.get('src', '')
+            
+            elem_json['stime'] = elem.find(class_='video-duration').get_text(strip=True) if elem.find(class_='video-duration') else ''
+            elem_json['quality'] = elem.find(class_=['badge-hd']).get_text(strip=True) if elem.find(class_=['badge-hd']) else ''
+            
+            
+            if elem.find('a',href=re.compile("/channels/[A-z0-9-]+(?:/|)")):
+                elem_json['canal'] = elem.find('a',href=re.compile("/channels/[A-z0-9-]+(?:/|)")).get_text(strip=True)
+                elem_json['title'] = elem_json['title'].replace('%s -' %elem_json['canal'], '')
+            pornstars = elem.find_all('a', href=re.compile("/pornstars/[A-z0-9-]+(?:/|)"))
+            if pornstars:
+                for x, value in enumerate(pornstars):
+                    pornstars[x] = value.get_text(strip=True)
+                elem_json['star'] = ' & '.join(pornstars)
+            
+        except:
+            logger.error(elem)
+            logger.error(traceback.format_exc())
+            continue
+        
+        if not elem_json['url']: continue
+        matches.append(elem_json.copy())
+    return matches
+
+
+def findvideos(item):
+    logger.info()
+    
+    return AlfaChannel.get_video_options(item, item.url, data='', matches_post=None, 
+                                         verify_links=False, findvideos_proc=True, **kwargs)
+
+
+def play(item):
+    logger.info()
+    itemlist = []
+    
+    soup = AlfaChannel.create_soup(item.url, **kwargs)
+    
+    url = soup.find('div', id='player').get('data-load', '')
+    
+    itemlist.append(Item(channel=item.channel, action="play", title= "%s", contentTitle = item.contentTitle, url=url))
+    itemlist = servertools.get_servers_itemlist(itemlist, lambda i: i.title % i.server.capitalize())
+    
+    return itemlist
+
+
+def search(item, texto, **AHkwargs):
+    logger.info()
+    kwargs.update(AHkwargs)
+    
+    item.url = "%ssearch?q=%s&page=1" % (host, texto.replace(" ", "+"))
+    
+    try:
+        if texto:
+            item.c_type = "search"
+            item.texto = texto
+            return list_all(item)
+        else:
+            return []
+    
+    # Se captura la excepción, para no interrumpir al buscador global si un canal falla
+    except:
+        for line in sys.exc_info():
+            logger.error("%s" % line)
+        return []
